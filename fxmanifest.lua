ae235eef91a43dd94ee9eed7c521bfc9d5f85817
Daniel GP <96537843+DaniGP17@users.noreply.github.com>
2024-03-31 22:06:05 +0200

    Bump mono flag

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,4 +9,4 @@ server_scripts {
     'server/bin/Release/**/*.net.dll',
 }
 
-mono_rt2 'Prerelease expiring 2024-03-31. See https://aka.cfx.re/mono-rt2-preview for info.'
+mono_rt2 'Prerelease expiring 2024-06-30. See https://aka.cfx.re/mono-rt2-preview for info.'
